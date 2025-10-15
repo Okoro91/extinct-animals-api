c82c8369f43fa04fdcbbf1f8465d73df4c208019
okoro <mi.fracri91@gmail.com>
2025-10-15 07:32:33 +0100

    improve card

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.getElementById("container");
+const img = "./img/animal.jpeg";
 let animalsData = [];
 const url = "https://extinct-api.herokuapp.com/api/v1/animal/20";
 console.log("Fetching data from:", url);
@@ -13,27 +14,33 @@ fetch(url)
   .catch((error) => console.error("Error fetching data:", error));
 
 const showAnimals = (animals) => {
-  console.log("Displaying animals:", animals[0].binomialName);
   container.innerHTML += `
              <div class="card">
+             <img src="${animals[0].imageSrc}" alt="${animals[0].commonName}" />
               <h2>${animals[0].binomialName}</h2>
               <h3>${animals[0].commonName}</h3>
               <p>Year Extinct: ${animals[0].lastRecord}</p>
               <p>Location: ${animals[0].location}</p>
               <p>Details: ${animals[0].shortDesc}</p>
-              <img src="${animals[0].imageSrc}" alt="${animals[0].commonName}" />
             </div>
           `;
 
   animals.map((animal) => {
     container.innerHTML += `
         <div class="card">
+        <img src="${
+          animal.imageSrc === "false" ? img : animal.imageSrc
+        }" alt="${animal.commonName}" />
          <h2>${animal.binomialName}</h2>
          <h3>${animal.commonName}</h3>
          <p>Year Extinct: ${animal.lastRecord}</p>
          <p>Location: ${animal.location}</p>
-         <p>Details: ${animal.shortDesc}</p>
-         <img src="${animal.imageSrc}" alt="${animals[0].commonName}" />
+         <p>Details: ${
+           animal.shortDesc.length === 100
+             ? animal.shortDesc
+             : animal.shortDesc.slice(0, 100) + "..."
+         }</p>
+         
        </div>
      `;
   });
